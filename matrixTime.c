5ce330c62c52cf1a731ccf0594f38e985402202f
Kaleb Byrum <kabyru@outlook.com>
2020-04-08 22:20:42 -0400

    Typos and things

--- matrixTime.c
+++ matrixTime.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-    clock_t start_t, end_t, total_t;
+    clock_t start_t, end_t;
     int sum = 0;
+    float totalInSec = 0.0;
 
     int matrix[20][20];
     for (int i = 0; i < 20; i++)
@@ -27,10 +28,10 @@ int main()
         }
     }
     end_t = clock();
-    print("End of the big loop, end_t = %ld\n", end_t);
+    printf("End of the big loop, end_t = %ld\n", end_t);
 
-    total_t = (double) (end_t = start_t) / CLOCKS_PER_SEC;
-    printf("Total time taken by CPU: %f\n", total_t);
+    totalInSec = (double) (end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Total time taken by CPU: %f\n", totalInSec);
     printf("Computed sum: %d\n",sum);
     printf("Exiting program...\n");
 
